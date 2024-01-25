f14b862ebbc6d95fbc14ff6f337d8c4f70d2d3d5
PSamardzhiev <petko9001@gmail.com>
2024-01-26 00:31:46 +0200

    Added Final Project Resources

---
+++ terraform/outputs.tf
@@ -0,0 +1,38 @@
+output "project-name" {
+  description = "Print a Custom Message:"
+  value       = "Telerik Academy UpSkill DevOps Project"
+}
+
+output "vpc_id" {
+  description = "Output the VPC ID"
+  value       = aws_vpc.vpc.id
+}
+
+output "public_url" {
+  description = "Public IRL for our Web Server"
+  value       = "https://${aws_instance.web_server.public_ip}:8080"
+}
+
+
+output "private_ip" {
+  description = "EC2 Private IP Address:"
+  value       = aws_instance.web_server.private_ip
+}
+
+output "public_ip" {
+  description = "Your EC2 Instance Public IP:"
+  value       = aws_instance.web_server.public_ip
+}
+output "vpc_information" {
+  description = "VPC Information about Environment"
+  value       = "Your ${aws_vpc.vpc.tags.Environment} VPC has an ID of ${aws_vpc.vpc.id}"
+}
+
+output "ec2-instance-name" {
+  description = "outputs your ec2-instance name"
+  value       = aws_instance.web_server.tags.Name
+  sensitive   = false
+}
+/*
+multiline comment 
+*/
---
+++ terraform/terraform.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">=1.0.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 2.1.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.0"
+    }
+  }
+}
